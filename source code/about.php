8d58e7e032c5556cfe99b02881c54b79434b45d0
HudaAljazzar <74008908+HudaAljazzar@users.noreply.github.com>
2020-11-05 18:54:56 +0200

    Create about.php

---
+++ source code/about.php
@@ -0,0 +1,27 @@
+<?php
+echo'<center><h1>  .. We Are Here To Help ..</h1></center>';
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <style>
+            
+            h1,h2{
+                background-color: black;
+                color: orange
+
+            }
+    </style>
+</head>
+<body>
+        </br></br></br>
+    <p><h3>We are a trading company that sells clothes for all ages from children to young people and both sexes (women and men) at reasonable prices and high quality and a month's guarantee, and we have a delivery service to all areas at low prices
+We are from the occupied Palestinian state from the Gaza Strip
+</h3></p>
+        </br></br>
+        <center><h2>We hope to be as you think</h2></center>  
+</body>
+</html>
+<?php require'footer.php';?>
