d105021d751e8a54b0579c8134131eb17e8de6dc
HudaAljazzar <74008908+HudaAljazzar@users.noreply.github.com>
2020-11-05 18:58:00 +0200

    Create connect.php

---
+++ source code/connect.php
@@ -0,0 +1,42 @@
+<?php
+echo'<center><h1>  .. We Are Here To Help ..</h1></center>';
+?>
+<html>
+    <head>
+        <style>
+            body{
+                background-color: white
+            }
+            h1{
+                background-color: black;
+                color: orange
+
+            }
+          a{
+              background-color: black;
+              color: orange;
+              width: 70;
+              height: 50
+           }
+           
+        </style>
+    </head>
+    <body>
+   
+<center>
+        </br></br></br></br>
+    <img src="images/feacbook.png" width="100" height="100"> &nbsp;&nbsp;&nbsp;&nbsp;
+    <img src="images/email.png" width="100" height="100"> &nbsp;&nbsp;
+    <img src="images/github.png" width="100" height="100"></br></br>&nbsp;&nbsp;&nbsp;
+    <a href="https://www.facebook.com/huda.aljazzar.1"> Click here</a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <a href="huda2130134@gmail.com"> Click here</a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <a href="https://github.com/HUDAALJAAZAR"> Click here</a> &nbsp;&nbsp;
+    
+</center>
+    
+
+    
+    
+    </body>
+</html>
+<?php require'footer.php';?>
