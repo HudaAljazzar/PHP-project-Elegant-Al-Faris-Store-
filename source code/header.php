828f7c7d84e50fe7b6017286a817849d559a05b2
HudaAljazzar <74008908+HudaAljazzar@users.noreply.github.com>
2020-11-05 19:03:38 +0200

    Create header.php

---
+++ source code/header.php
@@ -0,0 +1,21 @@
+<?php
+
+echo'    <center ><h1>^^  ..  Welcom To Elgant Al-Faris Store  ..  ^^</h1></center>';
+?>
+
+<html>
+<head>
+    <style>
+         h1{
+                color: orange;
+                text-align: center;
+                background-color: black;
+                padding:30px;
+              }
+           
+    </style>
+</head>
+<body>
+    
+</body>
+</html>
