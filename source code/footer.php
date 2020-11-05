04f47307e9cc585dd2bbec64094f64d0d40eca27
HudaAljazzar <74008908+HudaAljazzar@users.noreply.github.com>
2020-11-05 18:59:16 +0200

    Create footer.php

---
+++ source code/footer.php
@@ -0,0 +1,40 @@
+<?php
+echo'</br></br></br>';
+?>
+<Html>
+    <head> 
+    <style>
+        
+           /* Style The Dropdown Button */
+           .dropbtn {
+              background-color: black;
+              color: orange;
+              padding:12px;
+              font-size: 15px;
+              border:red;
+              cursor: pointer;
+            }
+
+
+        
+
+        /* Change color of dropdown links on hover */
+        .dropdown-content a:hover {background-color: white}
+
+        
+
+        /* Change the background color of the dropdown button when the dropdown content is shown */
+        .dropdown:hover .dropbtn {
+          background-color: white;
+        } 
+          
+</style>
+    </head>
+    <body><center>
+        <div class="dropdown">
+        <a class="dropbtn" href="Connect.php">Connect_With_Us</a>
+        <a class="dropbtn" href="about.php">Who_Are_We</a>
+        <a class="dropbtn" href="address.php">Address</a>
+    </center>
+    </body>
+</Html>
