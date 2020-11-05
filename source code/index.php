7674da6fd49dd7647d6f8fd2641e6e7a2da65f6f
HudaAljazzar <74008908+HudaAljazzar@users.noreply.github.com>
2020-11-05 19:04:08 +0200

    Create index.php

---
+++ source code/index.php
@@ -0,0 +1,50 @@
+<?php
+ require'header.php';
+ 
+?>
+<html>
+    <head>
+    <title> Elgant Al-Faris Store</title>
+        <style>
+            h1,img{
+                color: orange;
+                text-align: center;
+                background-color: black;
+                padding:15px;
+              }
+           h2{
+                color: orange;
+             }
+             h3{
+                 color:black;
+                 width: 85;
+                 height: 35;
+                 background-color:khaki
+             }
+           body {
+                
+                background-color: white;
+                color: orange;
+                }
+        </style>
+    </head>
+    <body>
+            </br>
+    <center><h2>Dear User PLease Sing up / Sing in</h2></br>
+    <a href="login.php"><h3>Sing In<h3></a>
+    <a href="singup.php"><h3>Sing Up</h3></a></center></br></br></br></br></br></br></br>
+
+        <img src="images/men.jpg" width="130" height="130">
+        <img src="images/huda.jpg" width="130" height="130">
+        <img src="images/child.jpg" width="130" height="130">
+        <img src="images/girls.jpg" width="130" height="130">
+        <img src="images/ima2.jpg" width="130" height="130">
+        <img src="images/mon.jpg" width="130" height="130">
+        <img src="images/moon.jpg" width="130" height="130">
+        <img src="images/cc.jpg" width="130" height="130">
+
+
+    </body>
+
+</html>
+<?php require'footer.php';?>
